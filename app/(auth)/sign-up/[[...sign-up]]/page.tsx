b861578f1d482ad8bbf24cdbc712c3c34842413e
FrameworkTV <email@example.com>
2025-12-29 19:42:49 +0100

    Update SignUp routing voor consistentie met SignIn

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -13,7 +13,8 @@ export default function SignUpPage() {
           </p>
         </div>
         <SignUp
-          routing="hash"
+          routing="path"
+          path="/sign-up"
           afterSignUpUrl="/onboarding/select-plan"
           afterSignInUrl="/dashboard"
         />
